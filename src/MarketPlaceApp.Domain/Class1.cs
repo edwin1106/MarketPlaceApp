abc6f26bf4955e16f56f8350d455c7596dcf92f8
Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
2020-03-19 15:40:10 -0500

    Adding Asset model
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>
    Co-Authored-By: Santiago Ríos Ríos <jaicob10@users.noreply.github.com>

--- src/MarketPlaceApp.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MarketPlaceApp.Domain
-{
-    public class Class1
-    {
-    }
-}
--- src/MarketPlaceApp.Domain/Models/Asset.cs
+++ src/MarketPlaceApp.Domain/Models/Asset.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace MarketPlaceApp.Domain.Models
 {
-    class Asset
+    public class Asset
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public  string Tag { get; set; }
+        [Required]
+        public string ImageURL { get; set; }
+
 
     }
 }
