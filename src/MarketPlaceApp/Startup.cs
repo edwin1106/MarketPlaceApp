ec63dd382d6a496f8361e79980761fad445e6732
Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
2020-03-19 15:22:13 -0500

    Integrating with Swagger
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>
    Co-Authored-By: Santiago Ríos Ríos <jaicob10@users.noreply.github.com>

--- src/MarketPlaceApp/Startup.cs
+++ src/MarketPlaceApp/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MarketPlaceApp
 {
@@ -26,6 +27,11 @@ namespace MarketPlaceApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,16 @@ namespace MarketPlaceApp
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseEndpoints(endpoints =>
             {
