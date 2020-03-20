50e762134b96f905708a2c9429a64a82cdc6e769
Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
2020-03-20 17:25:20 -0500

    Adding generic repositories
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>
    Co-Authored-By: Santiago Ríos Ríos <jaicob10@users.noreply.github.com>

---
+++ src/MarketPlaceApp.Domain/Infrastructure/Repositories/DataStorageExtended.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FamiliesApp.Domain.Infrastructure.Repositories
+{
+    public static class DataStorageExtended
+    {
+        public static IQueryable<TEntity> AddIncludes<TEntity>(
+            this IQueryable<TEntity> query,
+            params string[] includeProperties)
+                where TEntity : class
+        {
+            if (includeProperties != null)
+            {
+                foreach (var property in includeProperties)
+                {
+                    query = query.Include(property);
+                }
+            }
+
+            return query;
+        }
+    }
+}
