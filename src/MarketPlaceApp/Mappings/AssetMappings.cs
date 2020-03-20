8be70e5b404707e465b06cc874c1b9d8b522f70d
Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
2020-03-20 17:26:26 -0500

    Adding asset mappings
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>
    Co-Authored-By: Santiago Ríos Ríos <jaicob10@users.noreply.github.com>

---
+++ src/MarketPlaceApp/Mappings/AssetMappings.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MarketPlaceApp.Commands;
+using MarketPlaceApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketPlaceApp.Mappings
+{
+    public class AssetMappings : Profile
+    {
+        public AssetMappings()
+        {
+            CreateMap<ActualizarAsset, Asset>();
+            CreateMap<CrearAsset, Asset>();
+        }
+
+    }
+}
