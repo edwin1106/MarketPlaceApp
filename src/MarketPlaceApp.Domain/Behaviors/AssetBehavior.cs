c8c855461720412dd258d28e05ae5a5db68505b8
Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
2020-03-20 17:25:32 -0500

    Adding AssetBehavior
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>
    Co-Authored-By: Santiago Ríos Ríos <jaicob10@users.noreply.github.com>

---
+++ src/MarketPlaceApp.Domain/Behaviors/AssetBehavior.cs
@@ -0,0 +1,43 @@
+﻿using FamiliesApp.Domain.Infrastructure.Repositories;
+using MarketPlaceApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlaceApp.Domain.Behaviors
+{
+    public class AssetBehavior : IAssetBehavior
+    {
+        private readonly IDataStorage<Asset> _assetRepository;
+
+        public AssetBehavior(IDataStorage<Asset> assetRepository)
+        {
+            _assetRepository = assetRepository;
+        }
+
+        public async Task CreateAsync(Asset asset)
+        {
+            if (asset is null)
+            {
+                throw new ArgumentNullException(nameof(Asset));
+            }
+            await _assetRepository.InsertAsync(asset);
+        }
+
+        public async Task<List<Asset>> GetAllAsync()
+        {
+            return await _assetRepository.FindAllAsync();
+        }
+
+        public async Task<Asset> GetByIdAsync(int id)
+        {
+            return await _assetRepository.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task UpdateAsync(Asset asset)
+        {
+            await _assetRepository.UpdateAsync(asset);
+        }
+    }
+}
