f24c5561225dd2de74249488ad39b1e924f68b2e
Edwin Osorio <30908546+edwin1106@users.noreply.github.com>
2020-03-20 17:26:06 -0500

    Adding asset commands
    Co-Authored-By: Cristian David Castro Montes <castrom1998@gmail.com>
    Co-Authored-By: Santiago Ríos Ríos <jaicob10@users.noreply.github.com>

---
+++ src/MarketPlaceApp.Domain/Behaviors/IAssetBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MarketPlaceApp.Domain.Models;
+
+namespace MarketPlaceApp.Domain.Behaviors
+{
+    public interface IAssetBehavior
+    {
+        Task CreateAsync(Asset asset);
+        Task<List<Asset>> GetAllAsync();
+        Task<Asset> GetByIdAsync(int id);
+        Task UpdateAsync(Asset asset);
+    }
+}
---
+++ src/MarketPlaceApp.Domain/Infrastructure/Repositories/IDataStorage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamiliesApp.Domain.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Represents the most generic data storage.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IDataStorage<T> where T : class
+    {
+        Task<List<T>> FindAllAsync();
+
+        Task<List<T>> FindAllAsync(
+          params string[] includeProperties);
+
+        Task<List<T>> FindAsync(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            int skip = 0,
+            int take = 0);
+
+        Task<List<T>> FindAsync(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            int skip = 0,
+            int take = 0,
+            params string[] includeProperties);
+
+        Task<T> FirstOrDefaultAsync(
+            Expression<Func<T, bool>> filter = null);
+
+        Task<T> FirstOrDefaultAsync(
+            Expression<Func<T, bool>> filter = null,
+            params string[] includeProperties);
+
+        Task<int> Count(Expression<Func<T, bool>> filter = null);
+
+        Task InsertAsync(T entity);
+
+        Task InsertRangeAsync(IEnumerable<T> entities);
+
+        Task UpdateAsync(T entity);
+
+        Task UpdateRangeAsync(IEnumerable<T> entities);
+
+        Task DeleteAsync(T entity);
+
+        Task DeleteRangeAsync(IEnumerable<T> entities);
+
+
+    }
+}
